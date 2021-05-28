186de215a57bcf9f50c11abcf548917fff59255f
fuyao <fuyao@localhost.com>
2021-05-28 14:21:48 +0800

    初始

--- App.vue
+++ App.vue
@@ -1,6 +1,12 @@
 <script>
 export default {
 	onLaunch: function() {
+		uni.onTabBarMidButtonTap(() => {
+			console.log(333333)
+			uni.navigateTo({
+				url: 'pages/record/index'
+			});
+		});
 		console.log('App Launch');
 	},
 	onShow: function() {
@@ -14,5 +20,191 @@ export default {
 
 <style lang="scss">
 /*每个页面公共css */
+.hide {
+	display: none;
+}
+.show {
+	display: block;
+}
+.text-wrap {
+	word-wrap: break-word;
+}
+/*每个页面公共css */
+page {
+	font-family: PingFang SC;
+	color: #333333;
+	font-size: 26rpx;
+	width: 100%;
+	overflow-x: hidden;
+}
+image {
+	content: normal !important;
+	border: 0 none;
+	max-width: 100%;
+	height: auto;
+	letter-spacing: 0;
+}
+.main-container {
+	width: 100%;
+	/* #ifdef MP-WEIXIN */
+	min-height: 100vh;
+	/* #endif */
+	/* #ifdef H5 */
+	// min-height: 93.2vh;
+	min-height: 85vh;
+	/* #endif */
+	overflow-x: hidden;
+	box-sizing: border-box;
+}
+.text-ellipsis {
+	width: 100%;
+	display: inline-block;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	vertical-align: middle;
+}
+.text-ellipsis-two {
+	width: 100%;
+	// display: inline-block;
+	display: block;
+	display: -webkit-box;
+	word-wrap: break-word;
+	word-break: break-all;
+	-webkit-line-clamp: 2;
+	-webkit-box-orient: vertical;
+	overflow: hidden;
+	text-overflow: ellipsis;
+}
 
+.item-flex-between {
+	display: flex;
+	justify-content: space-between;
+}
+.item-flex-around {
+	display: flex;
+	justify-content: space-around;
+}
+.item-flex-start {
+	display: flex;
+	justify-content: flex-start;
+}
+.item-flex-end {
+	display: flex;
+	justify-content: flex-end;
+}
+.align-center {
+	align-items: center;
+}
+.align-middle {
+	vertical-align: middle;
+}
+
+.tip-text {
+	font-size: 26rpx;
+	color: #bcbcbe;
+	padding: 20rpx 0;
+}
+.list-bottom-tip-text {
+	display: block;
+	width: 100%;
+	font-size: 28rpx;
+	color: #bcbcbe;
+	box-sizing: border-box;
+	padding: 52rpx 10rpx;
+	text-align: center;
+}
+.data-load-text{
+	width: 100%;
+	font-size: 28rpx;
+	color: #b6b6b6;
+	text-align: center;
+	padding: 20rpx 0;
+}
+.center {
+	text-align: center;
+}
+.arrow-white {
+	&::before {
+		border-color: #fff !important;
+	}
+	&::after {
+		border-color: #fff !important;
+	}
+}
+.icon-arrow-right,
+.icon-arrow-down,
+.icon-arrow-up {
+	&::after {
+		content: '';
+		display: inline-block;
+		width: 12rpx;
+		height: 12rpx;
+		border-width: 0 3rpx 3rpx 0;
+		border-color: #797979;
+		border-style: solid;
+		vertical-align: middle;
+		margin-left: 10rpx;
+	}
+}
+.icon-arrow-left {
+	&::before {
+		content: '';
+		display: inline-block;
+		width: 12rpx;
+		height: 12rpx;
+		border-width: 0 3rpx 3rpx 0;
+		border-color: #797979;
+		border-style: solid;
+		vertical-align: middle;
+		transform: rotate(135deg);
+		margin-right: 15rpx;
+	}
+}
+.icon-arrow-right {
+	&::after {
+		transform: rotate(-45deg);
+	}
+}
+.icon-arrow-up {
+	&::after {
+		transform: rotate(-135deg);
+	}
+}
+.icon-arrow-down {
+	&::after {
+		transform: translateY(-7rpx) rotate(45deg);
+	}
+}
+
+.loader-box {
+	text-align: center;
+	padding: 40rpx 10rpx;
+}
+
+.loader-01 {
+	display: inline-block;
+	width: 25rpx;
+	height: 25rpx;
+	margin-right: 14rpx;
+	color: inherit;
+	vertical-align: middle;
+	pointer-events: none;
+	border: 4rpx solid #969799;
+	border-bottom-color: transparent;
+	border-radius: 50%;
+	animation: 1s loader-01 linear infinite;
+	position: relative;
+}
+
+@keyframes loader-01 {
+	0% {
+		-webkit-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	100% {
+		-webkit-transform: rotate(360deg);
+		transform: rotate(360deg);
+	}
+}
 </style>
--- pages/list/index.vue
+++ pages/list/index.vue
@@ -1,5 +1,29 @@
 <template>
-	<view class="content">
+	<view class="list-content">
+		<view class="list-header">
+			<view class="list-header-title item-flex-between">
+				<view><h-icon path="icon/date" width="50" /></view>
+				<view>{{ title }}</view>
+				<view><h-icon path="icon/more" width="50" /></view>
+			</view>
+			<view class="money-box">
+				<view class="list-budget">
+					<view class="number">{{ price.budget }}</view>
+					<view class="title">预算</view>
+				</view>
+				<view class="item-flex-around">
+					<view class="list-income">
+						<view class="number">{{ price.income }}</view>
+						<view class="title">收入</view>
+					</view>
+					<view class="list-expenses">
+						<view class="number">{{ price.expenses }}</view>
+						<view class="title">支出</view>
+					</view>
+				</view>
+			</view>
+		</view>
+
 		<view class="record-list">
 			<view v-for="(item, index) in list" :key="index" class="record-list-item" @click="onClickItem(item)">
 				<view class="record-list-item-header">
@@ -13,15 +37,15 @@
 					</view>
 				</view>
 
-				<view class="record-list-item-content">
+				<view class="record-list-item-content" v-for="(ele, eindex) in item.list" :key="eindex">
 					<view class="record-list-item-content-left">
-						<view class="record-list-item-icon"><h-icon :path="item.icon" width="30" /></view>
-						<view class="record-list-item-name">{{ item.name }}</view>
+						<view class="record-list-item-icon"><h-icon :path="'type/' + ele.icon" width="50" /></view>
+						<view class="record-list-item-name">{{ ele.name }}</view>
 					</view>
 
 					<view class="record-list-item-content-right">
-						<view class="record-list-item-category">{{ item.category == 'expenfiture' || item.category == 'balance' ? '-' : '' }}</view>
-						<view class="record-list-item-price">{{ item.price }}</view>
+						<view class="record-list-item-category">{{ ele.category == 'expenfiture' || ele.category == 'balance' ? '-' : '' }}</view>
+						<view class="record-list-item-price">{{ ele.price }}</view>
 					</view>
 				</view>
 			</view>
@@ -33,6 +57,12 @@
 export default {
 	data() {
 		return {
+			title: '今天又是贫穷的一天',
+			price: {
+				budget: 10,
+				income: 0,
+				expenses: 0
+			},
 			list: [],
 			total: 0
 		};
@@ -45,17 +75,40 @@ export default {
 		async getRecordList() {
 			try {
 				let { data } = await this.$api.getRecordList();
-				this.list = data.list;
+				// this.list = data.list;
 				this.total = data.total;
+				this.list = this.setList(data.list);
 				console.log('获取 明细列表', data);
 			} catch (e) {
 				console.error('获取 明细列表 失败', e);
 			}
 		},
 
+		setList(list) {
+			let result = [];
+			let index = 0;
+			for (let i in list) {
+				// console.log(23423, i, list[i]);
+				if (i > 0 && list[i].createDate == list[i - 1].createDate) {
+					result[index].list.push(list[i]);
+				} else {
+					index = i < 1 ? index : index + 1;
+					result[index] = { list: [] };
+					result[index].createDate = list[i].createDate;
+					result[index].createDay = list[i].createDay;
+					result[index].createTime = list[i].createTime;
+					result[index].category = list[i].category;
+					result[index].time = list[i].time;
+					result[index].list.push(list[i]);
+				}
+				result[index].totalPrice = +list[i].price;
+			}
+			return result;
+		},
+
 		/* 点击 列表某行数据 */
 		onClickItem(item) {
-			console.log(444,item)
+			console.log(444, item);
 			uni.navigateTo({ url: '/pages/list/detail?id=' + item.id });
 			// this.currentItem = item;
 		}
@@ -64,46 +117,103 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.record-list-item {
-	margin-bottom: 30rpx;
-	.record-list-item-header {
-		color: #999;
-		font-size: 30rpx;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		border-bottom: 1rpx solid #ececec;
-		padding: 15rpx 20rpx;
-		&-right {
-			display: flex;
-			.record-list-item-category {
-				margin-right: 15rpx;
+.list-content {
+	min-height: 92.5vh;
+	background: #f3f3f3;
+
+	.list-header {
+		position: relative;
+		width: 100%;
+		height: 400rpx;
+		color: #ffffff;
+		text-align: center;
+		background-image: url(../../static/img/cover/cover.jpg);
+		background-size: cover;
+		overflow: hidden;
+		padding: 20rpx;
+		box-sizing: border-box;
+		.list-header-title {
+			font-size: 30rpx;
+		}
+		.money-box {
+			position: absolute;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			width: 100%;
+			padding: 20rpx 0 30rpx;
+
+			.list-budget {
+				padding: 20rpx 0 40rpx;
+				.number {
+					font-size: 54rpx;
+				}
+			}
+			.list-income {
+				.number {
+					font-size: 28rpx;
+				}
+			}
+			.list-expenses {
+				.number {
+					font-size: 30rpx;
+				}
 			}
 		}
 	}
-	.record-list-item-content {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 20rpx 20rpx;
-		.record-list-item-icon {
-			width: 80rpx;
-			height: 80rpx;
-			margin: 0 auto;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			background: #ececec;
-			border-radius: 50%;
-			overflow: hidden;
-			margin-right: 10rpx;
-		}
-		&-left {
-			display: flex;
-			align-items: center;
-		}
-		&-right {
-			display: flex;
+
+	.record-list {
+		height: 62vh;
+		overflow-y: auto;
+		.record-list-item {
+			background: #ffffff;
+			padding: 20rpx;
+			margin: 20rpx 20rpx 30rpx;
+			box-shadow: 0 3rpx 20rpx rgba(#333333, 0.15);
+			border-radius: 20rpx;
+			.record-list-item-header {
+				color: #999;
+				font-size: 30rpx;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				border-bottom: 1rpx solid #ececec;
+				padding: 15rpx 20rpx;
+				&-right {
+					display: flex;
+					.record-list-item-category {
+						margin-right: 15rpx;
+					}
+				}
+			}
+			.record-list-item-content {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				padding: 20rpx 20rpx;
+				font-size: 28rpx;
+				.record-list-item-icon {
+					width: 80rpx;
+					height: 80rpx;
+					margin: 0 auto;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					background: #fcfcfc;
+					border-radius: 50%;
+					overflow: hidden;
+					margin-right: 10rpx;
+				}
+				&-left {
+					display: flex;
+					align-items: center;
+				}
+				&-right {
+					display: flex;
+					font-size: 34rpx;
+					font-weight: 600;
+				}
+			}
 		}
 	}
 }
